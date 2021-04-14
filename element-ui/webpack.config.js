5bf503a435fe49abe9f38f0dafcd6b896d57906b
liujie <2393650194@qq.com>
2021-04-14 14:35:59 +0800

    提交项目

---
+++ element-ui/webpack.config.js
@@ -0,0 +1,31 @@
+var path=require('path')
+var htmlWebpackPlugin=require('html-webpack-plugin')
+module.exports= {
+    entry: path.join(__dirname, './src/main.js'),//入口文件
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "bundle.js"
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'),
+            filename: 'index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            {test: /\.(jpg|png|gif|bmp|jpeg|pneg)$/, use: 'url-loader?limit=8000&name=[hash:8]-[name].[ext]'},
+            {test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader'},
+            {test: /\.js$/, use: 'babel-loader', exclude: /node_modules/},
+            {test: /\.vue$/, use: 'vue-loader'}
+        ]
+    },
+    resolve: {
+        alias: {
+            //"vue$":"vue/dist/vue.js"
+        }
+    }
+}
